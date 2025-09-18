8dc1f9551917c2faf6ed52ef574b0806d3e5b2b9
jurio5 <sia5492@naver.com>
2025-09-19 00:44:14 +0900

    [feat] @Timed 어노테이션을 통한 타메트릭 설정

---
+++ src/main/java/hello/order/v4/OrderServiceV4.java
@@ -0,0 +1,42 @@
+package hello.order.v4;
+
+import hello.order.OrderService;
+import io.micrometer.core.annotation.Timed;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+@Timed("my.order")
+public class OrderServiceV4 implements OrderService {
+
+    private AtomicInteger stock = new AtomicInteger(100);
+
+    @Override
+    public void order() {
+                            log.info("주문");
+                            stock.decrementAndGet();
+                            sleep(500);
+    }
+
+    @Override
+    public void cancel() {
+                            log.info("취소");
+                            stock.decrementAndGet();
+                            sleep(200);
+    }
+
+    @Override
+    public AtomicInteger getStock() {
+        return stock;
+    }
+
+    private static void sleep(int i) {
+        try {
+            Thread.sleep(i + new Random().nextInt(200) + 1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
